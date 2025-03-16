7f2eb3b58e49a0e7fa0ebd232ea6fe906ae5d70f
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-15 20:55:33 -0400

    feat: display MovieList on the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
+import MovieList from "./components/MovieList";
 
 export default function Home() {
   return (
     <div>
+      <MovieList />
     </div>
   );
 }
