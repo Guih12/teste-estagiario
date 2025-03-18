6abc4a29105ae904201f047a27e8002b537519a5
Fernando Washington <ironitabarbosa@gmail.com>
2025-03-17 23:37:17 -0400

    edit: remove some list elements

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -2,16 +2,17 @@ import './index.scss';
 
 export default function Navbar(){
     return(
-        <header>
-            <h1>Filmes</h1>
-            <nav className="navbar">
-                <ul>
-                    <li><a href="#">Início</a></li>
-                    <li><a href="#">Filmes</a></li>
-                    <li><a href="#">Contato</a></li>
-                </ul>
-            </nav>
-        </header>
+        <>
+            <div id='top'></div>
+            <header>
+                <h1>🍿SuperMarioCine</h1>
+                <nav className="navbar">
+                    <ul>
+                        <li><a href="#top">Início</a></li>
+                    </ul>
+                </nav>
+            </header>
+        </>
     )
 }
 
